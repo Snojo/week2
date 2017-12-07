cee7efde8538567ff7575072a4b6471c99f75f2f
Snojo <snorri13@ru.is>
2017-12-07 14:34:29 +0000

    Savior of the universe!

--- jenkinsProvisioning/jenkins-bootstrap.sh
+++ jenkinsProvisioning/jenkins-bootstrap.sh
@@ -5,16 +5,17 @@ exec > >(tee /var/log/user-data.log | logger -t user-data -s 2>/dev/console) 2>&
 DOCKER_USER=snojo
 DOCKER_PASS=Penzdc08
 
-sudo yum update
+
 sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+sudo yum update
 sudo yum -y remove java-1.7.0-openjdk
 sudo yum -y install java-1.8.0
 sudo yum -y install git
-sudo yum -y install pip
+#sudo yum -y install pip
 
 #NVM
-sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+#sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 
 
 sudo yum -y install docker
@@ -28,10 +29,5 @@ sudo usermod -a -G docker jenkins
 sudo service jenkins start
 sudo pip install docker-compose
 docker login --username=$DOCKER_USER --password=$DOCKER_PASS
-
-sudo npm install -g nodemon
-sudo npm install -g create-react-app
-nvm install 6.9.1
-nvm use 6.9.1
 
 touch ec2-init-done.markerfile
